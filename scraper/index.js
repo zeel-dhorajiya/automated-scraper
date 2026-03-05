6ef816d072f371eb522e6dd23b132f7b5937bb0d
Zeel Dhorajiya <zeeldhorajiya@gmail.com>
2026-03-05 21:14:36 +0530

    chore: remove testing fake data and enable live production monitoring

--- scraper/index.js
+++ scraper/index.js
@@ -285,9 +285,7 @@ async function run() {
         if (successCount > 0) {
             console.log(`Process completed with ${successCount}/4 updates successful.`);
 
-            // FAKE DATA FOR TESTING
-            newlyDiscoveredLinks.push({ type: 'spins' });
-            newlyDiscoveredLinks.push({ type: 'coins' });
+
 
             // Notify user of newly discovered links
             if (newlyDiscoveredLinks.length > 0) {
